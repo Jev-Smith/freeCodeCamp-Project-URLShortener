a0f7fe430d989a3503ef9297bec9323980a2cca4
Jevaughn Smith <smithjevaughn20@gmail.com>
2024-06-01 18:40:35 -0500

    Rewrote endpoints

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const dns = require('dns');
+const validURL = require('valid-url');
 const cors = require('cors');
 const app = express();
 
@@ -9,54 +9,45 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 const urlList = [];
-//middleware
-const createShortUrl = (req, res, next) => {
-  const url = req.body.url;
-  const domainName = url.substring(12);
-
-  dns.lookup(domainName, (err, value) => {
-    if(err || domainName.length === 0){
-      return res.status(404).json({error: 'invalid url'})
-    }
-
-    const isFound = urlList.includes(url);
-
-    if (!isFound) {
-      urlList.push(url);
-    }
-
-    const urlIndex = urlList.indexOf(url);
-
-    const urlObj = {
-      original_url: url,
-      short_url: urlIndex
-    }
-
-    req.urlObj = urlObj;
-    next();
-  })
-  
-}
-
 // Your first API endpoint
-app.post('/api/shorturl', createShortUrl, function(req, res) {
-  res.json(req.urlObj);
+app.post('/api/shorturl', function (req, res) {
+  const originalUrl = req.body.url;
+  const isValid = validURL.isWebUri(originalUrl);
+
+  if(isValid === undefined){
+    return res.json({error: 'invalid url'});
+  }
+
+  const isFound = urlList.includes(originalUrl);
+
+  if (!isFound) {
+    urlList.push(originalUrl);
+  }
+
+  const index = urlList.indexOf(originalUrl);
+
+  res.json(
+    {
+      original_url: originalUrl,
+      short_url: index
+    }
+  )
 });
 
-app.get('/api/shorturl/:index', function (req, res) {
-  const index = req.params.index;
-  res.redirect(urlList[index]);
+app.get('/api/shorturl/:short_url', function (req, res) {
+  const indexFromURL = req.params.short_url;
+  res.redirect(urlList[indexFromURL]);
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
