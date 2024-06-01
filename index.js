21fa7273d35b643bb25566c900e8e8044d53bcf6
Jevaughn Smith <smithjevaughn20@gmail.com>
2024-06-01 00:12:00 -0500

    Added required endpoints

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const dns = require('dns');
 const cors = require('cors');
 const app = express();
 
@@ -8,15 +9,52 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(express.urlencoded({extended: false}));
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const urlList = [];
+//middleware
+const createShortUrl = (req, res, next) => {
+  const url = req.body.url;
+  const domainName = url.substring(12);
+
+  dns.lookup(domainName, (err, value) => {
+    if(err || domainName.length === 0){
+      return res.status(404).json({error: 'invalid url'})
+    }
+
+    const isFound = urlList.includes(url);
+
+    if (!isFound) {
+      urlList.push(url);
+    }
+
+    const urlIndex = urlList.indexOf(url);
+
+    const urlObj = {
+      original_url: url,
+      short_url: urlIndex
+    }
+
+    req.urlObj = urlObj;
+    next();
+  })
+  
+}
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', createShortUrl, function(req, res) {
+  res.json(req.urlObj);
+});
+
+app.get('/api/shorturl/:index', function (req, res) {
+  const index = req.params.index;
+  res.redirect(urlList[index]);
 });
 
 app.listen(port, function() {
